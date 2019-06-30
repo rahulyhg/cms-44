c921d29725c2e48cb30938e4943b59daf9aef0bb
Mark LaDoux <mark.ladoux@gmail.com>
2019-06-30 11:30:30 -0500

    adjust db configuration to take in account different variable schemas used by the various components

--- config/db.example.php
+++ config/db.example.php
@@ -1,14 +1,29 @@
 <?php
 
+// Configuration schema expected by phinx.
+$db['phinx'] = [
+  'adapter'   => 'mysql',
+  'host'      => 'localhost',
+  'name'      => 'production_db',
+  'user'      => 'root',
+  'pass'      => '',
+  'port'      => '3306',
+  'charset'   => 'utf8mb4',
+  'collation' => 'utf8mb4_unicode_ci'
+];
+
+// Convert our migration config to follow the variable naming
+// scheme expected by doctrine.
+$db['doctrine'] = [
+  'driver'   => ['pdo_mysql'],
+  'user'     => $db['phinx']['user'],
+  'password' => $db['phinx']['pass'],
+  'dbname'   => $db['phinx']['name'],
+  'host'     => $db['phinx']['host'],
+  'charset'  => $db['phinx']['charset']
+];
+
 return [
-  'migrations' => [
-    'adapter'   => 'mysql',
-    'host'      => 'localhost',
-    'name'      => 'production_db',
-    'user'      => 'root',
-    'pass'      => '',
-    'port'      => '3306',
-    'charset'   => 'utf8mb4',
-    'collation' => 'utf8mb4_unicode_ci'
-  ],
+  'migrations' => $db['phinx'],
+  'database'   => $db['doctrine']
 ];
